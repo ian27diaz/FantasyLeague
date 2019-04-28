963d3734dc164b0b6b96c16e10e25a0b37d6f08b
Ian27Diaz <anand_ian@hotmail.com>
2019-04-28 14:45:59 -0500

    Pantalla Reglamento, miEquipo, header, login, y logout tienen detalles,

--- 2daEntrega/FantasyLeague/src/app/app-routing.module.ts
+++ 2daEntrega/FantasyLeague/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LogComponent } from './log/log.component';
+import { ReglamentoComponent } from './reglamento/reglamento.component';
+import { MiEquipoComponent } from './mi-equipo/mi-equipo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/authentication', pathMatch: 'full'},
+  {path: 'authentication', component: LogComponent},
+  {path: 'reglamento', component: ReglamentoComponent},
+  {path: 'miequipo', component: MiEquipoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ 2daEntrega/FantasyLeague/src/app/futbolistas/futbolista/Futbolista.ts
@@ -0,0 +1,13 @@
+export class Futbolista{
+
+    constructor(
+        public id: number,
+        public foto: string,
+        public edad: number,
+        public posiciones: string,
+        public precio: number,
+        public club: string,
+        public competicion: string,
+        public nacionalidad: string
+        ){}
+}

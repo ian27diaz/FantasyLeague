a8d53e32be5d7bd7d2ffaf0a77c61e38e83386c2
Ian27Diaz <anand_ian@hotmail.com>
2019-04-28 18:21:04 -0500

    añadi la vista de posiciones

--- 2daEntrega/FantasyLeague/src/app/app-routing.module.ts
+++ 2daEntrega/FantasyLeague/src/app/app-routing.module.ts
@@ -4,12 +4,26 @@ import { LogComponent } from './log/log.component';
 import { ReglamentoComponent } from './reglamento/reglamento.component';
 import { MiEquipoComponent } from './mi-equipo/mi-equipo.component';
 import { FutbolistasComponent } from './futbolistas/futbolistas.component';
+import { LigaComponent } from './liga/liga.component';
+import { EnfrentamientosComponent } from './Liga/enfrentamientos/enfrentamientos.component';
+import { TablaPosicionesComponent } from './Liga/tabla-posiciones/tabla-posiciones.component';
+
 const routes: Routes = [
   {path: '', redirectTo: '/authentication', pathMatch: 'full'},
   {path: 'authentication', component: LogComponent},
   {path: 'reglamento', component: ReglamentoComponent},
   {path: 'miequipo', component: MiEquipoComponent},
-  {path: 'futbolistaprueba', component: FutbolistasComponent}
+  {path: 'futbolistaprueba', component: FutbolistasComponent},
+  {path: 'liga', component: LigaComponent, children: [
+    {path: '', component: TablaPosicionesComponent},
+    {path: 'enfrentamientos', component: EnfrentamientosComponent, children: [
+      {path: '', component: EnfrentamientosComponent},
+      {path: 'enfrentamiento/:id', component: MiEquipoComponent}
+    ]},
+    {path: 'posiciones', component: TablaPosicionesComponent}
+  ]}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

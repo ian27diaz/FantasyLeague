5d9b9b17a439dfe83401ca912b80fc3ce5ba71ad
pallaro98 <is709389@iteso.mx>
2019-04-28 22:29:14 -0500

    Se agregó funcionalidad a la pantalla Lobby

--- 2daEntrega/FantasyLeague/src/app/lobby/lobby.component.ts
+++ 2daEntrega/FantasyLeague/src/app/lobby/lobby.component.ts
@@ -9,10 +9,41 @@ import { EquiposService } from '../mi-equipo/equipos.service';
 })
 export class LobbyComponent implements OnInit {
   equipos: Equipo[];
+  infoLiga = 1;
+  infoRubro = 1;
+  activeTabla = 'nav-item active';
+  activeEnfrentamientos = 'nav-item';
+  activeEstadisticas = 'nav-item';
+
   constructor(private equiposService: EquiposService) { }
 
   ngOnInit() {
     this.equipos = this.equiposService.getEquipo();
+  }
+
+  cambiarInfoLiga(ligaSel) {
+    this.infoLiga = ligaSel;
+  }
+
+  cambiarInfoRubro(rubroSel) {
+    this.infoRubro = rubroSel;
+    switch(rubroSel) {
+      case 1:
+        this.activeTabla = 'nav-item active';
+        this.activeEnfrentamientos = 'nav-item';
+        this.activeEstadisticas = 'nav-item';
+        break;
+      case 2:
+        this.activeTabla = 'nav-item';
+        this.activeEnfrentamientos = 'nav-item active';
+        this.activeEstadisticas = 'nav-item';
+        break;
+      case 3:
+        this.activeTabla = 'nav-item';
+        this.activeEnfrentamientos = 'nav-item';
+        this.activeEstadisticas = 'nav-item active';
+        break;
+    }
   }
 
 }
